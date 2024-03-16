6a38503dca08c4eae58f0bc390e8e671e263dba5
Mateus Robers Amaral <robersmateus@gmail.com>
2024-03-16 14:55:54 -0300

    treinando

--- casa.py
+++ casa.py
@@ -1,30 +1,10 @@
 import turtle
 import colorsys
 
-# Função para desenhar uma árvore
-def draw_tree(x, y):
-    t.penup()
-    t.goto(x, y)  # Posiciona a tartaruga
-    t.pendown()
-    t.color("brown")  # Define a cor da linha para o tronco
-    t.begin_fill()  # Começa a preencher a forma
-    t.left(90)
-    t.forward(50)  # Desenha o tronco da árvore
-    t.right(90)
-    t.color("green")  # Define a cor da linha para as folhas
-    t.begin_fill()  # Começa a preencher a forma
-    t.circle(50, 120)  # Desenha uma parte das folhas da árvore
-    t.left(120)
-    t.circle(50, 120)  # Desenha outra parte das folhas da árvore
-    t.end_fill()  # Termina de preencher a forma
-    t.right(120)
-    t.color("brown")  # Volta a cor da linha para o tronco
-    t.end_fill()  # Termina de preencher a forma
-
 # Configuração inicial
 t = turtle.Turtle()
 t.color("black")
-t.speed(2)  # Define a velocidade da tartaruga
+t.speed(10)  # Define a velocidade da tartaruga
 
 # Desenhar o retângulo principal da casa
 x_casa = -100
@@ -59,7 +39,7 @@ t.pendown()
 t.color("brown")
 t.begin_fill()
 for _ in range(2):
-    t.forward(largura_casa / 2)
+    t.forward(largura_casa / 4)
     t.left(90)
     t.forward(altura_casa / 2)
     t.left(90)
@@ -82,9 +62,49 @@ for _ in range(2):
     t.left(90)
 t.end_fill()  # Termina de preencher a forma
 
+t.speed(15)
+# Função para desenhar uma árvore
+def draw_tree(x, y):
+    t.penup()
+    t.goto(x, y)  # Posiciona a tartaruga
+    t.pendown()
+    t.color("brown")  # Define a cor da linha para o tronco
+    t.begin_fill()  # Começa a preencher a forma
+    t.left(90)
+    t.forward(50)  # Desenha o tronco da árvore
+    t.right(90)
+    t.left(90)
+    
+    t.color("green")  # Define a cor da linha para as folhas
+    t.begin_fill()  # Começa a preencher a forma
+    t.circle(50, 120)  # Desenha uma parte das folhas da árvore
+    t.left(30) #90
+    t.circle(50, 120)  # Desenha outra parte das folhas da árvore
+    t.end_fill()  # Termina de preencher a forma
+    t.right(30) #120
+    
+    t.color("green")  # Define a cor da linha para as folhas
+    t.begin_fill()
+    t.circle(50, 120)  # Desenha uma parte das folhas da árvore
+    t.left(30)
+    t.circle(50, 120)  # Desenha outra parte das folhas da árvore
+    t.end_fill()  # Termina de preencher a forma
+    t.right(30)
+
+    t.color("green")  # Define a cor da linha para as folhas
+    t.begin_fill()
+    t.circle(50, 120)  # Desenha uma parte das folhas da árvore
+    t.left(30)
+    t.circle(50, 120)  # Desenha outra parte das folhas da árvore
+    t.end_fill()  # Termina de preencher a forma
+    t.right(30)
+    
+    t.hideturtle()
+    t.end_fill()  # Termina de preencher a forma
+
 # Desenhar a árvore ao lado da casa
-x_arvore = x_casa + largura_casa + 50
-y_arvore = y_casa - 100
+x_arvore = x_casa + largura_casa + 150
+y_arvore = y_casa 
 draw_tree(x_arvore, y_arvore)
 
 def draw_one_color_arc(x,y,r,pensize,color):
@@ -104,7 +124,7 @@ turtle.title('Rainbow In Python Turtle')
 turtle.setup(700,700)
 num_colors = 49
 
-radius = 300
+radius = 500
 penwidth = 20*7/num_colors
 hue = 0
 
